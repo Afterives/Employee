3a7748e8ccdf848ded96288e4926ae257d012175
Afterives <rogprzemek9@gmail.com>
2022-06-26 22:36:18 +0200

    Initial commit

---
+++ Authentication/UserAccountService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace employee.Authentication
+{
+    public class UserAccountService
+    {
+        private List<UserAccount> _users;
+
+        public UserAccountService()
+        {
+            _users = new List<UserAccount>
+            {
+                new UserAccount{ UserName = "admin", Password = "admin", Role = "Administrator"},
+                new UserAccount{ UserName = "user", Password = "user", Role = "User"},
+                new UserAccount{ UserName = "Przemysław", Password = "przemek123", Role = "Administrator"}
+            };
+        }
+
+        public UserAccount? GetByUserName(string userName)
+        {
+            return _users.FirstOrDefault(x => x.UserName == userName);
+        }
+    }
+}
---
+++ Data/Models/UserMessage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace employee.Data.Models
+{
+    public class UserMessage
+    {
+        public string UserName { get; set; }
+        public string Message { get; set; }
+        public bool CurrentUser { get; set; }
+        public DateTime DateSent { get; set; }
+    }
+}
